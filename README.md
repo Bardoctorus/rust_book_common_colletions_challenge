493bc96e80107caee89bd284f4dc7a314452fef7
Bardoctorus <ianbuckley@Ians-Air.fritz.box>
2023-11-08 07:38:56 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Rust Book Chapter 8 Common Collections Challenges
+
+At the end of [Chapter 8 of the Rust Book](https://doc.rust-lang.org/book/ch08-03-hash-maps.html) There are a couple of coding challenges. This is just me banging my noob head against rust to solve them.
+
+If I'm feeling fancy I might iterate over it to show how I improve it but likely not.
+
+
---
+++ src/main.rs
@@ -0,0 +1,71 @@
+use rand::prelude::*;
+fn main() {
+    // Put random numbers in a Vec.
+    let mut in_vec : Vec<i32> = Vec::new();
+    let mut rng = thread_rng();
+    for _i in 0..15{
+    let x :i32 = rng.gen_range(1..30);
+    in_vec.push(x);
+    }
+    println!("in_vec is {:?}",in_vec);
+    //
+    //Now we start the challenge:
+    //Given a list of integers, use a vector and return the median (when sorted, the value in the middle position) and mode (the value that occurs most often; a hash map will be helpful here) of the list.
+    //First, Median.
+
+    in_vec.sort();
+    println!("Sorted in_vec is {:?}",in_vec);
+    println!("Sorted in_vec length is {}", in_vec.len());
+    //now, median is middle val... out of 15 though?
+    // A ha, school level textbook to the rescue:
+    // If the number of observations is odd, the number in the middle of the list is the
+    // let median  = in_vec.get((in_vec.len()+1)/2);  <-- this doesn't work as Vecs are 0
+    // indexed unlike real life
+    let opt_median  = in_vec.get(in_vec.len()/2);
+    let median;
+    match opt_median  {
+        Some(i) => median = i,
+        None => panic!("Couldn't get the median of in_vec: {:?}", in_vec),
+    };
+    println!("The median of Sorted in_vec is {}", median);
+    // Boom. Probably ugly and theres a better way to do it.
+    // Who cares.
+    // Now, what was next?
+    // Convert strings to pig latin. The first consonant of each word is moved to the end of the word and “ay” is added, so “first” becomes “irst-fay.” Words that start with a vowel have “hay” added to the end instead (“apple” becomes “apple-hay”). Keep in mind the details about UTF-8 encoding!
+    // Ok so I need some words
+    let mut words = String::from("Convert strings to pig latin. The first consonant of each word is moved to the end of the word and “ay” is added, so “first” becomes “irst-fay.” Words that start with a vowel have “hay” added to the end instead (“apple” becomes “apple-hay”). Keep in mind the details about UTF-8 encoding!");
+    println!("Words are {}",words);
+    // So, first we need to collect the words.
+    let mut word_vec : Vec<_>= words.split_whitespace().collect();
+    println!("word_vec is {:?}", word_vec);
+    // then we need to be able to access the length of each word and it's first letter
+    for word in word_vec {
+        println!("Word {} is {} characters long", word, word.len());
+        // i feel like an iterator makes more sense here so I can specify only taking the first
+        // char. It'll help with other character checking too
+        for c in word.chars(){
+            println!("First char in {} is {}", word, c);
+    // Now you need to check if first letter is consonant or vowel, or neither, and handle it
+            if c.is_alphanumeric(){
+                // match vowels and consants here
+                println!("TODO: make thie make sense: {}", pig_up(word.to_string()));
+            }
+            else {
+                // check the next letter to see if it's actually a letter
+                // Also, if the end of the word is a ! or ? or ) " any of these, it'll need moving
+                // to the new end. Handle all edge cases.
+            }
+            //this isn't the best way to do this but it works for now, iterator should replace it
+            break;
+        }
+    }
+    
+    // Then spit the Vec back out into a string.
+
+
+}
+    // Then move the correct letters (or not if it's a vowel) to the end with the -ay/-hay ting
+fn pig_up(input: String) -> String {
+    let ting = String::from("temp");
+    ting
+}
